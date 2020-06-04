1ebe52ffcef29ece99b627d3a45e2865f2368957
InesRachao <inesrachao@gmail.com>
2020-06-04 22:09:18 +0100

    react redux

--- server/models/citiesModel.js
+++ server/models/citiesModel.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 const city = new mongoose.Schema({
     name: {
         type: String,
-        required: true
+        required: true, 
+        unique: true
     },
     country: {
         type: String,
--- server/routes/cities.js
+++ server/routes/cities.js
@@ -2,18 +2,46 @@ const express = require("express");
 const router = express.Router();
 const cityModel = require("../models/citiesModel");
 
+
+
 router.get("/all", async (req, res) => {
     try {
         const cities = await cityModel.find()
-    
         res.send(cities)
         
     } catch (err) {
         res.status(500).send(err.message)
         
     }
-
 })
 
+
+router.post("/add", async (req, res) => {
+        const {
+            name,
+            country,
+            img,
+        } = req.body;
+        
+        try {
+            let city = await cityModel.findOne({name: req.body.name})
+            
+            if (city) {
+                res.status(400).json({errors: "city already exists"})
+            }
+            city = new cityModel({
+                name,
+                country,
+                img,
+            });
+
+        await city.save()
+        return res.json(city)
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).send("Server error");
+        }
+    }
+);
 
 module.exports = router
